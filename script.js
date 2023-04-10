3a2aa66e4ff688af0279d078405c05f85e14987d
Asliddin Amirov <asliddinamir2@gmail.com>
2023-04-10 17:11:22 +0800

    first push

---
+++ script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.btn')
+const author = document.querySelector('.author')
+const quote = document.querySelector('.quote')
+
+const api_url = 'https://type.fit/api/quotes'
+
+btn.addEventListener('click', btnClick)
+
+
+
+function btnClick (){
+    const random = Math.floor(Math.random() * 1643)
+
+    fetch(api_url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        quote.innerText = `"${data[random].text}"`
+        author.innerText = `--${data[random].author}--`
+    })
+
+}
